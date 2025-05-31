48beb636c8ee31aba3f9cfaf897b2a6c4ae1e424
Alfa06N <nicolasalfag@gmail.com>
2025-05-30 22:58:00 -0400

    Improve router

--- app/views/users/index.php
+++ app/views/users/index.php
@@ -10,7 +10,7 @@
 
     <title>Home</title>
 
-    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="app/views/users/Style.css">
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
